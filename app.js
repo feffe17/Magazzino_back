966e8d1c2f1d9fd571fe836d7bc449a3e9e509df
feffe1702 <fpm.asd@gmail.com>
2024-12-20 10:06:12 +0100

    [WIP] first run server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const host = process.env.HOST
+const port = process.env.PORT
+const cors = require('cors')
+
+
+app.get('/', (req, res) => {
+    res.send(`Server is up and running!`);
+})
+
+app.listen(port, () => {
+    console.log(`Server in ascolto a http://${host}:${port}`);
+})
